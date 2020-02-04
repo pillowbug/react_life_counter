7f807f26bd640c5b51ba65783d79e2fb5c3869ff
Gerard Cabarse <gerard.cabarse@gmail.com>
2020-02-04 11:07:05 -0800

    initial layout complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const players = [
+    {
+        name: "Nathan",
+        life: 40,
+    },
+    {
+        name: "Andre",
+        life: 40
+    },
+    {
+        name: "Gerard",
+        life: 40
+    },
+    {
+        name: "Nick",
+        life: 40
+    }
+]
